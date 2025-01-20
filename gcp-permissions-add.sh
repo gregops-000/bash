a2ade3e656520e8ee9f7fd1a0f3e9b1425ab7360
Greg W-Cha <102039238+gregops-000@users.noreply.github.com>
2025-01-20 14:18:30 +0100

    Update gcp-permissions-add.sh

--- gcp-permissions-add.sh
+++ gcp-permissions-add.sh
@@ -1,1 +1,42 @@
-gcp-permissions-add.sh
+#!/bin/bash
+
+PROJECT_ID="Project-ID-Project-ID"
+MEMBER="group:name_of_group@domain.com"
+
+# List of roles to assign
+ROLES=(
+    "roles/aiplatform.admin"
+    "roles/apigee.apiAdmin"
+    "roles/apigee.developerAdmin"
+    "roles/apigee.environmentAdmin"
+    "roles/backupdr.admin"
+    "roles/compute.imageUser"
+    "roles/compute.instanceAdmin.v1"
+    "roles/compute.networkAdmin"
+    "roles/compute.publicIpAdmin"
+    "roles/consumerprocurement.admin"
+    "roles/iam.serviceAccountCreator"
+    "roles/dataform.admin"
+    "roles/deloitte.cmsOperations"
+    "roles/notebooks.admin"
+    "roles/securitycenter.adminViewer"
+    "roles/iam.serviceAccountAdmin"
+    "roles/iam.serviceAccountUser"
+    "roles/storage.admin"
+    "roles/storage.folderAdmin"
+    "roles/storage.hmacKeyAdmin"
+    "roles/storage.objectAdmin"
+    "roles/storage.objectCreator"
+    "roles/storage.objectViewer"
+    "roles/vmmigration.admin"
+)
+
+for ROLE in "${ROLES[@]}"; do
+    echo "Assigning $ROLE to $MEMBER..."
+    gcloud projects add-iam-policy-binding "$PROJECT_ID" \
+        --member="$MEMBER" \
+        --role="$ROLE" \
+        --quiet
+done
+
+echo "All roles have been assigned."
